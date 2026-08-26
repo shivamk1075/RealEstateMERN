1ab68815b73f9da517a61e6536a2687235200fe6
Shivam Kumar <shivam.kumar.101075@gmail.com>
2026-08-26 20:40:04 +0530

    create header comp.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,19 @@ import Profile from './pages/Profile';
 import About from './pages/About';
 import Signin from './pages/Signin';
 import Signout from './pages/Signout';
+import Header from './components/Header';
 
 export default function App() {
-  return <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path="/profile" element={<Profile />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/signin" element={<Signin />} />
-    <Route path="/signout" element={<Signout />} />
-  </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+    <Header /> 
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signout" element={<Signout />} />
+    </Routes>
+    </BrowserRouter>
+    )
 }
