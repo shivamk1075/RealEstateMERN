f6df139fda57f7d520fe125d50e22c552a65db58
Shivam Kumar <shivam.kumar.101075@gmail.com>
2026-08-27 12:25:20 +0530

    complete sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import About from './pages/About';
 import Signin from './pages/Signin';
-import Signout from './pages/Signout';
+import Signup from './pages/Signup';
 import Header from './components/Header';
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
       <Route path="/profile" element={<Profile />} />
       <Route path="/about" element={<About />} />
       <Route path="/signin" element={<Signin />} />
-      <Route path="/signout" element={<Signout />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
     </BrowserRouter>
     )
