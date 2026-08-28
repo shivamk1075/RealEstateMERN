163884e0db7ed9d4dea0009210eb43f9991eccf1
Shivam Kumar <shivam.kumar.101075@gmail.com>
2026-08-28 19:32:13 +0530

    add update listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -27,4 +27,22 @@ export const deleteListing = async (req, res, next) => {
     } catch (error) {
         return next(error);
     }
-}
+};
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if(!listing){
+        return next(errorHandler(404,'Listing not found!'));
+    }
+
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401,'You can update only your own listing!'));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        return res.status(200).json(updatedListing);
+    } catch (error) {
+        return next(error);
+    }
+};
