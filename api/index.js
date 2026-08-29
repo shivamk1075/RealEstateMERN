3ec6a9f9510ba132a020a10989fc629fd220c189
Shivam Kumar <shivam.kumar.101075@gmail.com>
2026-08-29 12:10:50 +0530

    resolved syntax error in path implementation at backend index.js

--- api/index.js
+++ api/index.js
@@ -33,7 +33,10 @@ app.use("/api/listing", listingRouter);
 
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
-app.get('*', (req, res) => {
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
